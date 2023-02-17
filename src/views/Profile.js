f70bc740f2a93ba62c3000337e44a4dabf8dbf5a
EricOstoni <eostoni@student.unipu.hr>
2023-02-17 13:28:24 +0100

    Dodane Views

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,13 +1,45 @@
 import React from "react";
 
 // react-bootstrap components
+import { useState } from "react";
 import { Button, Card, Form, Container, Row, Col } from "react-bootstrap";
 import Appbar from "../components/Appbar";
 
 function User() {
+  const [name, SetName] = useState("");
+  const [address, SetAddress] = useState("");
+  const [prezime, SetPrezime] = useState("");
+  const [grad, SetGrad] = useState("");
+  const [drzava, SetDrzava] = useState("");
+  const [stanje_plinomjera, SetStanje_plinomjera] = useState("");
+  const [zipcode, SetZipcode] = useState("");
+  const [broj_plinomjera, SetBroj_plinomjera] = useState("");
+
+  const handlerClick = (e) => {
+    e.preventDefault();
+    const student = {
+      name,
+      address,
+      prezime,
+      grad,
+      drzava,
+      stanje_plinomjera,
+      zipcode,
+      broj_plinomjera,
+    };
+    console.log(student);
+    fetch("http://localhost:8080/student/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(student),
+    }).then(() => {
+      console.log("New student added");
+    });
+  };
+
   return (
     <>
-    <Appbar/>
+      <Appbar />
       <Container fluid>
         <Row>
           <Col>
@@ -24,6 +56,8 @@ function User() {
                         <Form.Control
                           placeholder="Prezime"
                           type="text"
+                          value={prezime}
+                          onChange={(e) => SetPrezime(e.target.value)}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -33,6 +67,8 @@ function User() {
                         <Form.Control
                           placeholder="Ime"
                           type="text"
+                          value={name}
+                          onChange={(e) => SetName(e.target.value)}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -44,6 +80,8 @@ function User() {
                         <Form.Control
                           placeholder="Kućna adresa"
                           type="text"
+                          value={address}
+                          onChange={(e) => SetAddress(e.target.value)}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -51,10 +89,12 @@ function User() {
                   <Row>
                     <Col className="pr-1" md="4">
                       <Form.Group>
-                        <label>Grad</label>
+                        <label v>Grad</label>
                         <Form.Control
                           placeholder="Grad"
                           type="text"
+                          value={grad}
+                          onChange={(e) => SetGrad(e.target.value)}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -64,6 +104,8 @@ function User() {
                         <Form.Control
                           placeholder="Država"
                           type="text"
+                          value={drzava}
+                          onChange={(e) => SetDrzava(e.target.value)}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -73,6 +115,8 @@ function User() {
                         <Form.Control
                           placeholder="ZIP Code"
                           type="number"
+                          value={zipcode}
+                          onChange={(e) => SetZipcode(e.target.value)}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -84,6 +128,8 @@ function User() {
                         <Form.Control
                           placeholder="Broj plinomijera"
                           type="number"
+                          value={broj_plinomjera}
+                          onChange={(e) => SetBroj_plinomjera(e.target.value)}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -95,6 +141,8 @@ function User() {
                         <Form.Control
                           placeholder="Stanje pliomjera"
                           type="number"
+                          value={stanje_plinomjera}
+                          onChange={(e) => SetStanje_plinomjera(e.target.value)}
                         ></Form.Control>
                       </Form.Group>
                     </Col>
@@ -104,6 +152,7 @@ function User() {
                     className="btn-fill pull-right"
                     type="submit"
                     variant="info"
+                    onClick={handlerClick}
                   >
                     Add Profile
                   </Button>
